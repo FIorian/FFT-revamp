f1471d9a65a46afac5296d544a4b466acbfe2a1b
Florian Gaeremynck <florian.gaeremynck@gmail.com>
2023-02-10 10:49:10 +0100

    adding cmake handles for glm, excluding libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include "pch.h"
+
 int main(int argc, char** argv)
 {
-    
+    glm::vec2 test{2.f,3.f};
+    std::cout << test.x << test.y << std::endl;
+    return 0;
 }
---
+++ src/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// UTILS
+#include <iostream>
+
+// EXTERNAL LIBRARIES
+#include <glm/glm.hpp>
